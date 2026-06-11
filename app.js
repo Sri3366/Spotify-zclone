d9d12c6acfea75660b8e7f392755a22ce478b99d
Sri3366 <srivenkatchigurusetti3@gmail.com>
2026-06-11 22:34:35 +0530

    spotify-clone

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const shuffle = document.getElementById('shuffle');
 const repeat = document.getElementById('repeat');
 const nowBar = document.querySelector('.now-bar');
 
-let audio = new Audio('Audio/1.mp3');
+let audio = new Audio('songs/Majboor_Sheheryar_Rehan_x_Zoha_Waseem_Music_Video.mp3');
 let currentSongIndex = 0; // Modernized to standard 0-indexed tracks pointer
 let songOnRepeat = false;
 let songOnShuffle = false;
@@ -20,22 +20,22 @@ let songOnShuffle = false;
 const songs = [
     { id: 1, songName: 'Massa Massa', songDes: 'Movie:Peddi', songImage: 'images/peddi.webp', songPath: 'songs/Massa Massa (Telugu Version) - SouthMelody.mp3' },
     { id: 2, songName: 'Majboor', songDes: 'Movie:Majboor', songImage: 'images/majboor.jpg', songPath: 'songs/Majboor_Sheheryar_Rehan_x_Zoha_Waseem_Music_Video.mp3' },
-    { id: 3, songName: 'Naal Nachna', songDes: 'Movie:Durandhar-Ranveer,Sara Arjun', songImage: 'images/Naal nachna.jpg', songPath: 'songs/Naal Nachna Dhurandhar 320 Kbps.mp3' },
-    { id: 4, songName: 'Raag of Revenge', songDes: 'Movie:DC-Lokesh kanagaraj,Wamiqa Gabbi', songImage: 'images/Raaga of Revenge.jpg', songPath: 'songs/Raga of Revenge.mp3' },
-    { id: 5, songName: 'Aaya Sher', songDes: 'Movie:Paradise-Nani,Kayadu lohar', songImage: 'images/Aaaya sher.jpg', songPath: 'songs/Aaya Sher.mp3' },
-    { id: 6, songName: 'Make Way for the King', songDes: 'This is the description for song 6', songImage: 'images/Raaka.jpg', songPath: 'songs/Make-Way-For-The-King-Sai-Abhyankkar-NaaSongs.mp3' },
+    { id: 3, songName: 'Naal Nachna', songDes: 'Movie:Durandhar  Ranveer,Sara Arjun ', songImage: 'images/Naal nachna.jpg', songPath: 'songs/Naal Nachna Dhurandhar 320 Kbps.mp3' },
+    { id: 4, songName: 'Raag of Revenge', songDes: 'Movie:DC  Lokesh kanagaraj ,Wamiqa Gabbi ', songImage: 'images/Raaga of Revenge.jpg', songPath: 'songs/Raga of Revenge.mp3' },
+    { id: 5, songName: 'Aaya Sher', songDes: 'Movie:Paradise  Nani,Kayadu lohar ', songImage: 'images/Aaaya sher.jpg', songPath: 'songs/Aaya Sher.mp3' },
+    { id: 6, songName: 'Make Way for the King', songDes: 'Movie:Raaka Bhaai, Deepika', songImage: 'images/Raaka.jpg', songPath: 'songs/Make-Way-For-The-King-Sai-Abhyankkar-NaaSongs.mp3' },
     { id: 7, songName: 'Sahiba', songDes: 'Private song', songImage: 'images/sahiba.jpg', songPath: 'songs/Sahiba(KoshalWorld.Com).mp3' },
-    { id: 8, songName: 'Singari', songDes: 'Movie:Dude-Pradeep Ranganathan,MamithaBaiju', songImage: 'images/singari.jpg', songPath: 'songs/Singari (Telugu)(KoshalWorld.Com).mp3' },
+    { id: 8, songName: 'Singari', songDes: 'Movie:Dude Pradeep Ranganathan,MamithaBaiju', songImage: 'images/singari.jpg', songPath: 'songs/Singari (Telugu)(KoshalWorld.Com).mp3' },
     { id: 9, songName: 'Sundari', songDes: 'Singer:Sanjay Rathod', songImage: 'images/Sundari.jpg', songPath: 'songs/Sundari(KoshalWorld.Com).mp3' },
-    { id: 10, songName: 'Shararat', songDes: 'Movie:Durandhar-Ranveer,Sara Arjun', songImage: 'images/shararat.jpg', songPath: 'songs/Shararat Dhurandhar 128 Kbps.mp3' },
+    { id: 10, songName: 'Shararat', songDes: 'Movie:Durandhar Ranveer,Sara Arjun', songImage: 'images/shararat.jpg', songPath: 'songs/Shararat Dhurandhar 128 Kbps.mp3' },
     { id: 11, songName: 'Shaky', songDes: 'Singer:Sanjay Rathod', songImage: 'images/shaky.jpg', songPath: 'songs/Shaky(KoshalWorld.Com).mp3' },
     { id: 12, songName: 'Manohari', songDes: 'Movie:Bahubali(Hindi)', songImage: 'images/manohari.jpg', songPath: 'songs/Manohari (PenduJatt.Com.Se).mp3' },
-    { id: 13, songName: 'Hellallallo', songDes: 'Movie:Peddi-RC,janvi', songImage: 'images/Hellallallo.jpg', songPath: 'songs/Hellallallo (Telugu)(KoshalWorld.Com).mp3' },
-    { id: 14, songName: 'Adiye', songDes: 'Movie:Bachelor-GV,Divya bharathi', songImage: 'images/Adiye.jpg', songPath: 'songs/Adiye-MassTamilan.fm.mp3' },
+    { id: 13, songName: 'Hellallallo', songDes: 'Movie:Peddi  RC,janvi ', songImage: 'images/Hellallallo.jpg', songPath: 'songs/Hellallallo (Telugu)(KoshalWorld.Com).mp3' },
+    { id: 14, songName: 'Adiye', songDes: 'Movie:Bachelor  GV,Divya bharathi ', songImage: 'images/Adiye.jpg', songPath: 'songs/Adiye-MassTamilan.fm.mp3' },
     { id: 15, songName: 'Dippam Dappam', songDes: 'Movie:Kanmani-Rambo-Khatija', songImage: 'images/Dippam dappam.jpg', songPath: 'songs/Dippam Dappam(KoshalWorld.Com).mp3' },
-    { id: 16, songName: 'Monica', songDes: 'Movie:Coolie,RajiniKanth,Nagarjuna', songImage: 'images/monica.jpg', songPath: 'songs/Monica (Telugu)(KoshalWorld.Com).mp3' },
+    { id: 16, songName: 'Monica', songDes: 'Movie:Coolie  RajiniKanth,Nagarjuna ', songImage: 'images/monica.jpg', songPath: 'songs/Monica (Telugu)(KoshalWorld.Com).mp3' },
     { id: 17, songName: 'Golden Sparrow', songDes: 'Movie:Jabillama Neeku Antha Kopama', songImage: 'images/Golden sparrow.jpg', songPath: 'songs/Golden Sparrow(KoshalWorld.Com).mp3' },
-    { id: 18, songName: 'Chuttamalle', songDes: 'Movie:Devara-NTR,Janvi', songImage: 'images/chuttamalle.jpg', songPath: 'songs/Chuttamalle(KoshalWorld.Com).mp3' }
+    { id: 18, songName: 'Chuttamalle', songDes: 'Movie:Devara  NTR,Janvi ', songImage: 'images/chuttamalle.jpg', songPath: 'songs/Chuttamalle(KoshalWorld.Com).mp3' }
 ];
 
 // Active runtime deployment queue (cloned from original on boot initialization)
